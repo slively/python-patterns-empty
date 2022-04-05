5af8154fabaacda6ee22be6a5206d9a28797a4ae
Scott Lively <scott@revl.com>
2022-04-05 16:04:21 -0700

    stuff

--- src/file_syncer/main/dir_synchronizer_ctrl.py
+++ src/file_syncer/main/dir_synchronizer_ctrl.py
@@ -1,9 +1,16 @@
+from logging import getLogger
+import os
 from src.file_syncer.main.base_dir_synchronizer import BaseDirSynchronizer
 from typing import List
 from src.file_syncer.main.directory_model import DirectoryChangesModel
 
+log = getLogger(__name__)
+
 
 class DirSynchronizerCtrl(BaseDirSynchronizer):
+
+    _changes: List[DirectoryChangesModel] = []
+
     def __init__(self, dst_dir: str) -> None:
         self.dir = dst_dir
 
@@ -11,10 +18,29 @@ class DirSynchronizerCtrl(BaseDirSynchronizer):
         """
         Synchronize a set of directory changes to some other directory.
         """
-        pass
+        if not changes.changes_detected:
+            return
+
+        self._changes.append(changes)
+
+        # deleted files
+        for file in changes.deleted_files:
+            full_path = os.path.join(self.dir, file.path)
+            log.info("Synchronizing delete %s", file.path)
+            if file.is_dir:
+                os.rmdir(full_path)
+            else:
+                os.remove(full_path)
+
+        # new and changed files
+        for file in changes.new_files + changes.changed_files:
+            full_path = os.path.join(self.dir, file.path)
+            log.info("Synchronizing change %s", file.path)
+            if file.is_dir:
+                os.mkdir(full_path)
+            else:
+                with open(full_path, "w+") as f:
+                    f.write(file.contents)
 
     def list_changes(self) -> List[DirectoryChangesModel]:
-        """
-        List all changes received so far, will not return events that contain no changes.
-        """
-        pass
+        return self._changes
