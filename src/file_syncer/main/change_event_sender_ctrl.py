f98772b63330e7960811f1bea6b884d65374fce7
Scott Lively <scott@revl.com>
2022-04-05 15:54:50 -0700

    stuff

--- src/file_syncer/main/change_event_sender_ctrl.py
+++ src/file_syncer/main/change_event_sender_ctrl.py
@@ -1,9 +1,10 @@
 from abc import abstractmethod
 from queue import Queue
 from src.file_syncer.main.directory_model import DirectoryChangesModel
+from src.file_syncer.main.base_change_event_sender import BaseChangeEventSender
 
 
-class DirChangeEventCtrl:
+class DirChangeEventCtrl(BaseChangeEventSender):
 
     def __init__(self, queue: Queue) -> None:
         self.queue = queue
