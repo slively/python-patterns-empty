e521afafb578ef401541dee87e3b4a612c990d0c
Scott Lively <scott@revl.com>
2022-04-05 15:44:03 -0700

    stuff

--- src/file_syncer/main/base_change_event_sender.py
+++ src/file_syncer/main/base_change_event_sender.py
@@ -2,12 +2,12 @@ from abc import abstractmethod
 from src.file_syncer.main.directory_model import DirectoryChangesModel
 
 
-class BaseDirChangeEvent:
+class BaseChangeEventSender:
     """
     A simple interface for sending directory change events.
     Default implementation is a no-op.
     """
 
     @abstractmethod
-    def send_event(self, event: DirectoryChangesModel) -> None:
+    def send(self, event: DirectoryChangesModel) -> None:
         pass
